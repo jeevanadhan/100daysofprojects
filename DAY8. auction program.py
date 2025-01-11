90c1d8da88a2b5a04a879bf1c2f9d49506054773
jeevanadhan <jeevanadhan8014@gmail.com>
2025-01-11 12:59:40 +0530

    #coffe machine project

--- DAY8. auction program.py
+++ DAY8. auction program.py
@@ -1,12 +1,8 @@
 def is_leap_year(year):
     # Write your code here.
     # Don't change the function name.
-    if year % 4 == 0:
-        if year % 100 == 0 and year % 400 == 0:
-            return "True"
-        elif year % 100!=0:
-            return "True"
-    return "False"
+    if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+        return True
+    return False
 
-
-print(is_leap_year(int(input(""))))
+print(is_leap_year(int(input("Enter a year: "))))
