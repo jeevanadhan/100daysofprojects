9d9fa571d47e73db600980baf66a0067c0f45362
Jeevanadhan <jeevanadhan8014@gmail.com>
2025-02-08 18:27:42 +0530

    Add files via upload

--- DAY22.US state game/main.py
+++ DAY22.US state game/main.py
@@ -1,35 +1,43 @@
-# import csv,pandas
-# with open("weather_condition.csv") as file:
-#     data=pandas.read_csv(file)
-#     print(data)
-#     temp_list=data["temp"].to_list()
-#     print(temp_list)
-#     total=0
-#     avg=sum(temp_list)/len(temp_list)
-#     print(f"average = {round(avg,2)}")
-#     max_value=data["temp"].max()
-#     print(f"the maximum value is {max_value}")
-#     monday=data[data.day == "Monday"]
-#     monday_temp=monday.temp[0]
-#     far= (monday_temp * 9 / 5) + 32
-#     print(f"the fahrenhiet of monday = {far}")
-import pandas
-from collections import Counter
-with open("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv") as file:
-    data=pandas.read_csv(file)
-    gray_count=len(data[data["Primary Fur Color"]=="Gray"])
-    cinnamon_count=len(data[data["Primary Fur Color"]=="Gray"])
-    black_count=len(data[data["Primary Fur Color"]=="Gray"])
-    data_dict={ "fur color":["gray","cinnamon","black"],
-                "Count":[gray_count,cinnamon_count,black_count]}
-    df=pandas.DataFrame(data_dict)
-    df.to_csv("squirrel_color_count.csv")
+import turtle,pandas
+screen=turtle.Screen()
+img="blank_states_img.gif"
+screen.addshape(img)
+turtle.shape(img)
+with open("50_states.csv") as file:
+    data = pandas.read_csv(file)
+    data_dict = data.to_dict()
+
+    state_list = data["state"].to_list()
+game_over=False
+guessed=0
+guessed_states=[]
+while not game_over:
+    user_answer=screen.textinput(title=f"{guessed}/50  Guess the state",prompt="whats the another state name?")
+    if user_answer is None:
+        break
+    elif user_answer=="exit":
+        break
 
 
+    if user_answer.title() in state_list:
+        guessed_states.append(user_answer)
+        state_index = state_list.index(user_answer.title())
+        xcor=data_dict["x"][state_index]
+        ycor=data_dict["y"][state_index]
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(xcor,ycor)
+        t.write(arg=str(user_answer),font=("Arial", 7, "bold"))
+        guessed+=1
+        state_list.remove(state_list[state_index])
+    if guessed>=50:
 
-
-
-
-
-    # values_count=data_dict.values()
-    # print(values_count)
+        game_over =True
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(0,0)
+        t.write(arg="YOU WON",align="center",font=("Aerial",24,"bold"))
+df=pandas.DataFrame(state_list)
+df.to_csv("states_to_learn.csv")
