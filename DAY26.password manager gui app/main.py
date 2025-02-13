1a52b313b5144783fe0c747073d6bd0348f0afd0
Jeevanadhan <jeevanadhan8014@gmail.com>
2025-02-13 18:42:59 +0530

    Add files via upload

--- DAY26.password manager gui app/main.py
+++ DAY26.password manager gui app/main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window=Tk()
+window.title("PASSWORD MANAGER")
+window.config(padx=20,pady=20)
+
+
+canvas=Canvas(width=200,height=224,highlightthickness=0)
+photoimage=PhotoImage(file="logo.png")
+canvas.create_image(100,112,image=photoimage)
+canvas.grid(row=0,column=1)
+
+window.mainloop()
