db5f88ebb8a8f39698b62f53566d449aa554f1c8
Jeevanadhan <jeevanadhan8014@gmail.com>
2025-02-17 21:33:18 +0530

    Add files via upload

--- DAY26.password manager gui app/main.py
+++ DAY26.password manager gui app/main.py
@@ -19,13 +19,18 @@ website_text=Label(text="website:", font=("Arial", 10, "bold"))
 website_text.grid(column=0,row=4)
 website_text.config(pady=10)
 website_entry=Entry(width=50)
-website_entry.grid(column=1,row=4)
+website_entry.grid(column=1,row=4,columnspan=2)
 email_text=Label(text="Email/Username:", font=("Arial", 10, "bold"))
-email_text.grid(column=0,row=5,columnspan=1)
+email_text.grid(column=0,row=5)
 email_entry=Entry(width=50)
-email_entry.grid(column=1,row=5)
-
-
+email_entry.grid(column=1,row=5,columnspan=2)
+password_text=Label(text="password:", font=("Arial", 10, "bold"))
+password_text.grid(column=0,row=6)
+password_entry=Entry(width=21)
+password_entry.grid(column=1,row=6,columnspan=2)
+password_text.config(pady=10)
+generate_pass_button=Button(text="Generate password")
+generate_pass_button.grid(column=1,row=6)
 
 
 window.mainloop()
