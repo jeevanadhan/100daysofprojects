19eef0c27502d51cf2e988c8157c5e178a42ada7
itadori <jeevanadhan8014@gmail.com>
2025-03-05 18:52:41 +0530

    Add files via upload

--- DAY36.workout tracker using API/main.py
+++ DAY36.workout tracker using API/main.py
@@ -1,10 +1,10 @@
 import requests
 from datetime import datetime
 import os
-APP_ID="9acdae3a"
-APP_KEY="72ef75be67278af52970399dffe33ed2"
-NUTRITIONIX_ENDPOINT='https://trackapi.nutritionix.com/v2/natural/exercise'
-SHEETY_ENDPOINT="https://api.sheety.co/0878ff3d6090c64de5156589cb31c2db/workoutSheet/workouts"
+APP_ID=os.environ.get("APP_ID")
+APP_KEY=os.environ.get("APP_KEY")
+NUTRITIONIX_ENDPOINT=os.environ.get("NUTRITIONIX_ENDPOINT")
+SHEETY_ENDPOINT=os.environ.get("SHEETY_ENDPOINT")
 my_params={
     "query":input("tell me which exercise you did: ")
 }
@@ -18,7 +18,6 @@ formatted_time=datetime.now().strftime("%H,%M,%S")
 response=requests.post(url=NUTRITIONIX_ENDPOINT,json=my_params,headers=header)
 response.raise_for_status()
 data=response.json()
-print(data)
 if "exercises" in data and len(data["exercises"]) >0:
     exercise_name=data["exercises"][0]["name"]
     duration=data["exercises"][0]["duration_min"]
@@ -28,19 +27,18 @@ else:
     print("no exercise data found")
     exit()
 workout_params={
-    "workout":{
-        "Date":formatted_date,
-        "Time":formatted_time,
-        "Exercise":exercise_name,
-        "Duration":duration,
-        "Calories":calories
+    "sheet1":{
+        "date":formatted_date,
+        "time":formatted_time,
+        "exercise":exercise_name,
+        "duration":duration,
+        "calories":calories
     }
 }
-token=os.environ.get('BEARER_TOKEN')
+token=os.environ.get("BEARER_TOKEN")
 header1={
-    "Authorization":f"Bearer dmfkdjfjakdnffkljadflkndjfnaksdfndjdnfjksadndfnadkjfdasdjfaA"
+    "Authorization":f"Bearer {token}"
 }
-
 sheety_response=requests.post(url=SHEETY_ENDPOINT,json=workout_params,headers=header1)
 print("Sheety API Response:", sheety_response.text)
 
